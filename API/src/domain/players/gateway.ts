e906c134a41c077812e9622dfd5aad9ca2d79ffe
cjumelin <cjumelin@protonmail.com>
2023-02-22 01:46:12 +0100

    feat(getPlayers): Players retrieves from "api"
        - ADR: Should I test ?

--- API/src/domain/players/gateway.ts
+++ API/src/domain/players/gateway.ts
@@ -1,6 +1,23 @@
-import { players } from "./stub";
+// ############################################################################
+// TODO => Quick fix to solve https://stackoverflow.com/questions/69041454/error-require-of-es-modules-is-not-supported-when-importing-node-fetch
+const _importDynamic = new Function("modulePath", "return import(modulePath)");
+export const fetch = async function (...args: any) {
+  const { default: fetch } = await _importDynamic("node-fetch");
+  return fetch(...args);
+};
+// ############################################################################
+
+const apiURL =
+  "https://eurosportdigital.github.io/eurosport-node-developer-recruitment/headtohead.json";
+
+const playersFetcher = async (): Promise<Player[]> => {
+  const res = await fetch(apiURL);
+  return (await res.json()).players;
+};
 
 export const playerGateway = {
-  retrieveAll: () =>
-    players.sort((playerA, playerB) => playerA.id - playerB.id),
+  retrieveAll: async () => {
+    const players = await playersFetcher();
+    return players.sort((playerA, playerB) => playerA.id - playerB.id); // query ?sort=id not working :(
+  },
 };
